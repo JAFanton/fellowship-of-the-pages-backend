e0f45499f2251e32b05021d1a9b3943f34ce3337
Justin Fanton <justin.fanton@gmail.com>
2025-01-02 10:08:59 +0100

    Adjusted book route

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use("/api", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-const bookRoutes = require("./routes/book.routes");
-app.use("/books", bookRoutes);
+const bookRouter = require("./routes/book.routes");
+app.use("/api", bookRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
