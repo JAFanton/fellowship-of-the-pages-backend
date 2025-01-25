ef31c6737c8e9cac006a28bd7c697e26575f3b41
Justin Fanton <justin.fanton@gmail.com>
2025-01-25 12:43:29 +0100

    Fixed conflict with jwt

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -1,41 +1,17 @@
-const { expressjwt: jwt } = require("express-jwt");
+const jwt = require("jsonwebtoken")
 
-if (!process.env.TOKEN_SECRET) {
-  throw new Error("TOKEN_SECRET is not defined in the environment variables");
-}
 
-// Instantiate the JWT token validation middleware
-const isAuthenticated = jwt({
-  secret: process.env.TOKEN_SECRET,
-  algorithms: ["HS256"],
-  requestProperty: "payload",
-  getToken: getTokenFromHeaders,
-});
-
-// Function used to extract the JWT token from the request's 'Authorization' Headers
-function getTokenFromHeaders(req) {
-  // Check if the token is available on the request Headers
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    // Get the encoded token string and return it
+const isAuthenticated = (req, res, next) => {
+  try {
     const token = req.headers.authorization.split(" ")[1];
-    return token;
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    req.payload = payload;
+
+    next();
+  } catch (error) {
+    res.status(401).json("token not provided or not valid");
   }
-  console.error("No token found in Authorization headers");
-  return null;
 }
 
-const handleJWTError = (err, req, res, next) => {
-  if (err.name === "UnauthorizedError") {
-      return res.status(401).json({ error: "Invalid or missing token" });
-  }
-  next(err);
-};
-
-// Export the middleware so that we can use it to create protected routes
-module.exports = {
-  isAuthenticated,
-  handleJWTError,
-};
+module.exports = { isAuthenticated }
