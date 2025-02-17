704f6ad0fc5ac288d1ae914bdac8c0247ac07531
Justin Fanton <justin.fanton@gmail.com>
2025-02-17 13:50:56 +0100

    new health check route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,23 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+// Health check for connection
+router.get('/health', (req, res) => {
+  // send ping to prevent inactivity on mongodb atlas
+  mongoose.connection.db.admin().ping()
+    .then( () => {
+      res.status(200).json({
+        status: 'ok',
+        timestamp: new Date().toISOString()
+      });
+    })
+    .catch(err => {
+      console.error('MongoDB ping failed:', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Failed to connect to MongoDB',
+      });
+    });
+});
+
 module.exports = router;
